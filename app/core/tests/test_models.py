d772a7755ebb7ef0a83e716f825128d43cc9e184
Rommel Rodriguez Perez <rommelrodperez@gmail.com>
2023-01-06 19:59:41 -0500

    Some changes to core's models and test_models
    Affected files:
    	modified:   app/core/models.py
    	modified:   app/core/tests/test_models.py

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -25,7 +25,7 @@ class ModelTests(TestCase):
 
         sample_emails = [ 
             ['test1@EXAMPLE.com', 'test1@example.com'],
-            ['Test2@Example.com', 'test2@example.com'],
+            ['Test2@Example.com', 'Test2@example.com'],
             ['TEST3@EXAMPLE.COM', 'TEST3@example.com'],
             ['test4@EXAMPLE.COM', 'test4@example.com'],
         ]
@@ -33,3 +33,21 @@ class ModelTests(TestCase):
         for email, expected in sample_emails:
             user = get_user_model().objects.create_user(email, 'sample123')
             self.assertEqual(user.email, expected)
+
+    def test_new_user_without_email_raises_error(self):
+        """ Test that creating a user without an email raises a ValueError """
+
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user('', 'test123')
+
+    def test_create_superuser(self):
+        """ Test creating a superuser """
+
+        user = get_user_model().objects.create_superuser(
+            'test@example.com',
+            'test123',
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
+
