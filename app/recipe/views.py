3ccaa3123164f15abc73ff8c0fc215ba4e8947e6
Rommel Rodriguez Perez <rommelrodperez@gmail.com>
2023-02-10 21:12:08 -0500

    Added tests and modified the ModelViewSet in recipe
    Everything working fine (tested) up to the implementation of the unit
    tests. The modifications to the ModelViewSet, as well as the new
    RecipeDetailSerializer, is yet to be tested.
    
    	modified:   app/recipe/serializers.py
    	modified:   app/recipe/tests/test_recipe_api.py
    	modified:   app/recipe/views.py

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -12,7 +12,7 @@ from recipe import serializers
 
 class RecipeViewSet(viewsets.ModelViewSet):
     """ View for manage recipe APIs """
-    serializer_class = serializers.RecipeSerializer
+    serializer_class = serializers.RecipeDetailSerializer
     # NOTE: Read a bit more about queryset
     queryset = Recipe.objects.all()
     authentication_classes = [TokenAuthentication]
@@ -23,3 +23,10 @@ class RecipeViewSet(viewsets.ModelViewSet):
         # NOTE: This method needed to be overridden, in order
         # to filter the recipes by the authenticated user 
         return self.queryset.filter(user=self.request.user).order_by('-id')
+
+    def get_serializer_class(self):
+        """ Return the serializer class for request """
+        if self.action == 'list':
+            return serializers.RecipeSerializer
+
+        return self.serializer_class
