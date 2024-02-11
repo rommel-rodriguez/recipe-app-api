27b612795ab2fef81491c87a10dab3f3ad8c491c
Rommel Rodriguez Perez <rommelrodperez@gmail.com>
2024-02-10 21:11:39 -0500

    Tags listing functionality implemented

--- app/recipe/urls.py
+++ app/recipe/urls.py
@@ -1,21 +1,17 @@
 """
 URL mapping for the recipe app
 """
-from django.urls import (
-    path,
-    include,
-)
 
+from django.urls import include, path
+from recipe import views
 from rest_framework.routers import DefaultRouter
 
-from recipe import views
-
-
 router = DefaultRouter()
-router.register(r'recipes', views.RecipeViewSet)
+router.register(r"recipes", views.RecipeViewSet)
+router.register(r"tags", views.TagViewset)
 
-app_name = 'recipe'
+app_name = "recipe"
 
 urlpatterns = [
-    path('',include(router.urls)),
+    path("", include(router.urls)),
 ]
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -2,16 +2,16 @@
 Views for the recipe APIs
 """
 
-from rest_framework import viewsets
+from core.models import Recipe, Tag
+from recipe import serializers
+from rest_framework import mixins, viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from core.models import Recipe
-from recipe import serializers
-
 
 class RecipeViewSet(viewsets.ModelViewSet):
-    """ View for manage recipe APIs """
+    """View to manage recipe APIs"""
+
     serializer_class = serializers.RecipeDetailSerializer
     # NOTE: Read a bit more about queryset
     queryset = Recipe.objects.all()
@@ -19,22 +19,40 @@ class RecipeViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
-        """ Retrieve recipes for authenticated user """
+        """Retrieve recipes for authenticated user"""
         # NOTE: This method needed to be overridden, in order
-        # to filter the recipes by the authenticated user 
-        return self.queryset.filter(user=self.request.user).order_by('-id')
+        # to filter the recipes by the authenticated user
+        return self.queryset.filter(user=self.request.user).order_by("-id")
 
     def get_serializer_class(self):
-        """ Return the serializer class for request """
+        """Return the serializer class for request"""
         # NOTE: We override this method in order for it to address 2 endpoints
-        # instead of just one. This way, the 
-        if self.action == 'list':
+        # instead of just one. This way, the
+        if self.action == "list":
             return serializers.RecipeSerializer
 
         return self.serializer_class
 
     def perform_create(self, serializer):
-        """ Create a new recipe """
-        # NOTE: This method overrides Django's behavior for saving a Model 
+        """Create a new recipe"""
+        # NOTE: This method overrides Django's behavior for saving a Model
         # object.
+        serializer.save(user=self.request.user)
+
+
+class TagViewset(mixins.ListModelMixin, viewsets.GenericViewSet):
+    """Manage tags in the database."""
+
+    serializer_class = serializers.TagSerializer
+    queryset = Tag.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """Overriding the DRF's default to scope it to the owner user"""
+
+        return self.queryset.filter(user=self.request.user).order_by("-name")
+
+    def perform_create(self, serializer):
+        """Create a new tag"""
         serializer.save(user=self.request.user)
